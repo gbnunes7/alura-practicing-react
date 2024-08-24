0b235877932fdc86ebbf53f350a398c8e9f69160
Gabriel Melo <bielrj170@gmail.com>
2024-08-24 20:40:56 -0300

    style: change style for link

--- src/components/headerLink/index.jsx
+++ src/components/headerLink/index.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const HeaderLink = ({url,children}) => {
     return (
-        <Link to={url} className="text-white text-lg">
+        <Link to={url} className="text-white text-lg p-4 no-underline">
             {children}
         </Link>
     )
