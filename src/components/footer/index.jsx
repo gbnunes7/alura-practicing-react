eb1686202863b1e23c46bd9846b4448ff86f39d5
Gabriel Melo <bielrj170@gmail.com>
2024-08-24 20:46:46 -0300

    feat: add a footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-red-500 p-4 flex justify-center items-center">
+            <p className="text-white text-lg">Desenvolvido por Gabriel Melo, em curso da Alura.</p>
+        </footer>
+    )
+}
+
+export default Footer
