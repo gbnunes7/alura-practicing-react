59a4c57749083b967650b83c70c8ccb600ecfb43
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 00:08:27 -0300

    feat: add nested route to apply FixedLayout to all child routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,18 @@
 import Home from "../pages/home";
 import Error404 from "../pages/error";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FixedLayout from "../components/fixedLayout";
 
 function AppRoutes() {
 	return (
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home/>}/>
-					<Route path="/*" element={<Error404/>}/>
-				</Routes>
-			</BrowserRouter>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<FixedLayout/>}>
+					<Route path="/" element={<Home />} />
+					<Route path="/*" element={<Error404 />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
