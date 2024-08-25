9f2aba8b022dbe73936a4ec577a9629844180b1f
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 02:32:54 -0300

    feat: add logic to show favorites card in page favorite

--- src/pages/favorite/index.jsx
+++ src/pages/favorite/index.jsx
@@ -4,17 +4,29 @@ import Title from "../../components/title";
 import { useCardContext } from "../../hooks/useCardContext";
 
 const Favorite = () => {
-    const {card} = useCardContext()
+	const { favoritosFiltrados, aoClicar, favorito } = useCardContext();
 	return (
 		<>
 			<Banner backgroundImage={"/public/imagens/banner-favoritos.png"} />
-            <Title>
-                <h2>Meus Favoritos</h2>
-            </Title>
-            <section className="flex justify-center gap-4 align-center p-6">
-				{card.map((item, index) => (
-					<Card key={index} src={item.capa} title={item.titulo} alt={item.titulo} />
-				))}
+			<Title>
+				<h2>Meus Favoritos</h2>
+			</Title>
+			<section className="flex justify-center gap-4 p-4 align-center">
+				{favoritosFiltrados.length > 0 ? (
+					favoritosFiltrados.map((cardItem) => (
+						<Card
+							key={cardItem.id}
+							id={cardItem.id}
+							title={cardItem.titulo}
+							src={cardItem.capa}
+							alt={cardItem.titulo}
+							aoClicar={aoClicar}
+							favorito={favorito[cardItem.id]} //
+						/>
+					))
+				) : (
+					<p>Você ainda não adicionou nenhum titulo ao seus favoritos.</p> //
+				)}
 			</section>
 		</>
 	);
