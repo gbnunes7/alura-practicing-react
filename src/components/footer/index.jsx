b979e0b84a7a6bdc6638a775fa38a47233eb7f8e
Gabriel Melo <bielrj170@gmail.com>
2024-08-24 23:36:01 -0300

    style: fix height and padding style to footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="bg-red-500 p-4 flex justify-center items-center">
+        <footer className="bg-black flex justify-center items-center h-22 p-2">
             <p className="text-white text-lg">Desenvolvido por Gabriel Melo, em curso da Alura.</p>
         </footer>
     )
