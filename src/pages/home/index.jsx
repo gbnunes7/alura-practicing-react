34885b5f28f5f2310b8faef29a87eae69361112c
Gabriel Melo <bielrj170@gmail.com>
2024-08-24 23:41:53 -0300

    feat: add a home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,17 @@
-const Home = () => {
-    return (
-        <p>Olá, mundo!</p>
-    )
-}
+import Banner from "../../components/banner";
+import Footer from "../../components/footer";
+import Header from "../../components/header";
+import Main from "../../components/Main";
 
-export default Home
+const Home = () => {
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Header />
+			<Banner backgroundImage={"/public/imagens/banner-home.png"} />
+			<Main/>
+			<Footer />
+		</div>
+	);
+};
+
+export default Home;
