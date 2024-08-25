336ea6208631e9533e0439aff50ed1a90853088c
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 16:19:33 -0300

    feat: add link to player  when click on img card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,11 @@
-const Card = ({ title, src, alt, aoClicar, favorito,id }) => {
+import { Link } from "react-router-dom";
+
+const Card = ({ title, src, alt, aoClicar, favorito, id }) => {
 	return (
 		<figure className="w-52 bg-gray-300 flex flex-col rounded shadow-md shadow-gray-400">
-			<img src={src} alt={alt} className="h-54 object-cover" />
+			<Link to={`/${id}`}>
+				<img src={src} alt={alt} className="h-54 object-cover" />
+			</Link>
 			<figcaption className="font-bold text-center text-md p-2 h-16">
 				{title}
 			</figcaption>
