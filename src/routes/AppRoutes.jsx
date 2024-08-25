76a2abd376d062023298838151bcd04976fd1f97
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 01:26:08 -0300

    feat: add a card provider wrapping all routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,16 +2,19 @@ import Home from "../pages/home";
 import Error404 from "../pages/error";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FixedLayout from "../components/fixedLayout";
+import { CardProvider } from "../context/CardContext";
 
 function AppRoutes() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<FixedLayout/>}>
-					<Route path="/" element={<Home />} />
-					<Route path="/*" element={<Error404 />} />
-				</Route>
-			</Routes>
+			<CardProvider>
+				<Routes>
+					<Route path="/" element={<FixedLayout />}>
+						<Route path="/" element={<Home />} />
+						<Route path="/*" element={<Error404 />} />
+					</Route>
+				</Routes>
+			</CardProvider>
 		</BrowserRouter>
 	);
 }
