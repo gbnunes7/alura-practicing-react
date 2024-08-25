92a0a991180e58eda77784fbd1596d5c9062b152
Gabriel Melo <bielrj170@gmail.com>
2024-08-24 23:13:12 -0300

    style: fix card style

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,11 +2,11 @@ import favIconMarcado from "./favorite.png";
 
 const Card = ({ title, src, alt }) => {
 	return (
-		<figure className="w-52 pb-4 bg-gray-300 flex flex-col gap-2 rounded shadow-md shadow-gray-400">
-			<img src={src} alt={alt} className="flex-grow" />
-			<div className="p-2 flex flex-col gap-4">
-				<figcaption className="font-bold">{title}</figcaption>
-				<img src={favIconMarcado} alt="Icone de favorito" className="w-5" />
+		<figure className="w-52 bg-gray-300 flex flex-col rounded shadow-md shadow-gray-400">
+			<img src={src} alt={alt} className="h-54 object-cover" />
+			<figcaption className="font-bold text-center text-md p-2 h-16">{title}</figcaption>
+			<div className="flex justify-between items-center px-2 pb-2">
+				<img src={favIconMarcado} alt="Ícone de favorito" className="w-5" />
 			</div>
 		</figure>
 	);
