2e16bd42b7666e70ec402fafa2d60c754e47a42e
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 03:02:42 -0300

    feat: add a underline style when page is selected

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "./logo.png";
 import HeaderLink from "../headerLink";
 
 const Header = () => {
+	const locate = useLocation();
+
+	const getUnderlineStyle = (path) => {
+		return location.pathname === path ? "underline" : "none";
+	};
+
 	return (
 		<header className="bg-black flex justify-between py-5 px-96 items-center">
 			<Link>
-            <img src={logo} alt="Logo cinetag"/>
-            </Link>
+				<img src={logo} alt="Logo cinetag" />
+			</Link>
 			<nav>
-				<HeaderLink url="./">
+				<HeaderLink url="./" underline={getUnderlineStyle("/")}>
 					Home
 				</HeaderLink>
-                <HeaderLink url="./favoritos">
+				<HeaderLink
+					url="./favoritos"
+					underline={getUnderlineStyle("/favoritos")}
+				>
 					Favoritos
 				</HeaderLink>
 			</nav>
