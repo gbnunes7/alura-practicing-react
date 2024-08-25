c31f867d5773b17fc51bbc5b87de9952669499db
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 02:53:07 -0300

    feat: add a button to back page

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,7 +1,14 @@
-const Error404 = () => {
-    return (
-        <p>Essa página não existe</p>
-    )
-}
+import { useNavigate } from "react-router-dom";
 
-export default Error404
+const Error404 = () => {
+	const navigate = useNavigate();
+
+	return (
+		<div className="flex flex-col items-center gap-8 m-12">
+			<p>Essa página não existe</p>
+			<button className="bg-blue-400 text-white py-1 px-4 rounded transition duration-300 ease-in-out hover:bg-blue-700" onClick={() => navigate(-1)}>Voltar</button>
+		</div>
+	);
+};
+
+export default Error404;
