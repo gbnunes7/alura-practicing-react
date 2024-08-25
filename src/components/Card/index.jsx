1de91eed42f4635ec70c793a47b4ffcfb107b307
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 00:49:11 -0300

    feat: add logic to toggle favorite button

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import favIconMarcado from "./favorite.png";
+import favIconDesmarcado from "./favorite_outline.png";
+
 
 const Card = ({ title, src, alt }) => {
+	const [favorito, setFavorito] = useState(favIconDesmarcado)
+	
+	function aoClicar() {
+		setFavorito(favorito === favIconDesmarcado ? favIconMarcado : favIconDesmarcado);
+	}
+
+
 	return (
 		<figure className="w-52 bg-gray-300 flex flex-col rounded shadow-md shadow-gray-400">
 			<img src={src} alt={alt} className="h-54 object-cover" />
 			<figcaption className="font-bold text-center text-md p-2 h-16">{title}</figcaption>
 			<div className="flex justify-between items-center px-2 pb-2">
-				<img src={favIconMarcado} alt="Ícone de favorito" className="w-5" />
+				<button onClick={aoClicar}><img src={favorito} alt="Ícone de favorito" className="w-5"/></button>
 			</div>
 		</figure>
 	);
