6a9703a9750c5eab750243411acbb86434a0e49e
Gabriel Melo <bielrj170@gmail.com>
2024-08-25 00:06:26 -0300

    refactor: move header and footer component to fixedLayout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,12 @@
 import Banner from "../../components/banner";
-import Footer from "../../components/footer";
-import Header from "../../components/header";
 import Main from "../../components/Main";
 
 const Home = () => {
 	return (
-		<div className="flex flex-col min-h-screen">
-			<Header />
+		<>
 			<Banner backgroundImage={"/public/imagens/banner-home.png"} />
-			<Main/>
-			<Footer />
-		</div>
+			<Main />
+		</>
 	);
 };
 
